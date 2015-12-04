a31d5961f3cbc8eb49a435b01fd134bd30d27a85
Lê Đức Thịnh <le.duc.thinh.24081994@gmail.com>
2015-12-04 11:42:36 +0700

    t

--- js/scope_hoiting.js
+++ js/scope_hoiting.js
@@ -1,6 +1,20 @@
-var globalScope = 'global_scope';
+(function (){
+	"use strict";
+	var globalScope = 'global_scope';
 intit();
 function intit() {
 	var functionScope = "function_Scope";
-		
-	}
+		var i;
+		var object = {
+			name: "name",
+			age: 12
+		};
+		console.log(object);
+		console.log(JSON.stringify(object));
+		console.log(i);
+		for(var i=0; i<3;i++){
+			console.log(i);
+		}
+		console.log(i);
+	};
+})();
